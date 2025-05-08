59cc9cfcf3ced6ca1325f80b67bcb79de58e1df6
Taruna Asthana <hiyasrivastava777@gmail.com>
2025-05-09 02:45:29 +0530

    Updated project

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -52,9 +52,9 @@ function Navbar() {
 
         <li><Link to="home" smooth={true} duration={500} spy={true} activeClass="active" offset={-70} >Home</Link></li>
         <li><Link to="about" smooth={true} duration={500} spy={true} activeClass="active" offset={-70} >About</Link></li>
+        <li><Link to="contact" smooth={true} duration={500} spy={true} activeClass="active" offset={-70} >Contact</Link></li>
         <li><Link to="skills" smooth={true} duration={500} spy={true} activeClass="active" offset={-70} >Skills</Link></li>
         <li><Link to="projects" smooth={true} duration={500} spy={true} activeClass="active" offset={-70} >Projects</Link></li>
-        <li><Link to="contact" smooth={true} duration={500} spy={true} activeClass="active" offset={-70} >Contact</Link></li>
       </ul>
 
 
